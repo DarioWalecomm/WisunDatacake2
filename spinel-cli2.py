dbcf454cb9f23e3fee843a2ba0926a99130c6123
DarioWalecomm <148904419+DarioWalecomm@users.noreply.github.com>
2023-10-30 17:26:16 -0600

    Add files via upload

--- spinel-cli2.py
+++ spinel-cli2.py
@@ -788,7 +788,10 @@ class SpinelCliCmd(Cmd, SpinelCodec):
                                 print("No CoAP payload")
                             else:
                                 print("Raw CoAP payload: {}".format(list(p.payload)))
-                                var = p.payload
+                                var = list.(p.payload)
+                                print(var)
+                                uplink = var[1: -1]
+                                print(uplink)
                                 client = paho.mqtt.client.Client(client_id="python", clean_session=False)
                                 client.tls_set()
                                 client.on_connect = on_connect
